68538efeaa9cdf33e230d83c07941e7c78e86cb9
DOP <workplace0594@ukr.net>
2016-03-23 00:10:27 +0200

    Change in Views

--- SportsComplex/SportsComplex.Database/views.sql
+++ SportsComplex/SportsComplex.Database/views.sql
@@ -1,14 +1,14 @@
 USE [SportsComplex];
 GO
 
-ALTER VIEW vPriceList 
+CREATE VIEW vPriceList 
 AS
 	SELECT c.Id, c.ClassTypeId as [TypeId], ct.Name as [TypeName], c.Area, c.Rate
 	FROM [tblClass] c
 	INNER JOIN [tblClassType] ct  ON c.ClassTypeId = ct.Id;
 GO
 
-ALTER VIEW vFreeSportsHalls
+CREATE VIEW vFreeSportsHalls
 AS
 	SELECT c.Id, c.ClassTypeId as [TypeId], ct.Name as [TypeName], c.Area, c.Rate, c.[Status]
 	FROM [tblClass] c
